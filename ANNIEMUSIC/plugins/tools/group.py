65583e3109b1804d077f023c4fb8007a12dc86ff
natha <108113193+Mandaiya@users.noreply.github.com>
2025-05-23 00:25:33 +0530

    Update group.py

--- ANNIEMUSIC/plugins/tools/group.py
+++ ANNIEMUSIC/plugins/tools/group.py
@@ -7,12 +7,12 @@ from config import OWNER_ID
 
 @app.on_message(filters.video_chat_started)
 async def on_voice_chat_started(_, message: Message):
-    await message.reply_text("🎙 **ᴠᴏɪᴄᴇ ᴄʜᴀᴛ ʜᴀs sᴛᴀʀᴛᴇᴅ!**")
+    await message.reply_text("🎙 **𝙾𝚗 𝙿𝚊𝚗𝚗𝚒𝚝𝚊𝚗𝚐𝚊 𝙳𝚊𝚠𝚠 𝙾𝚗 𝙿𝚊𝚗𝚗𝚒𝚝𝚊𝚗𝚐𝚊𝚊**")
 
 
 @app.on_message(filters.video_chat_ended)
 async def on_voice_chat_ended(_, message: Message):
-    await message.reply_text("🔕 **ᴠᴏɪᴄᴇ ᴄʜᴀᴛ ᴇɴᴅᴇᴅ.**")
+    await message.reply_text("🔕 **𝙸𝚗𝚊𝚗𝚐𝚊 𝚕𝚎𝚢𝚢 𝚊𝚝𝚑𝚞𝚔𝚞𝚕𝚊𝚒𝚢𝚎𝚎 𝚌𝚕𝚘𝚜𝚎 𝚙𝚊𝚗𝚗𝚒𝚝𝚎𝚗𝚐𝚊.**")
 
 
 @app.on_message(filters.video_chat_members_invited)
@@ -28,10 +28,10 @@ async def on_voice_chat_members_invited(_, message: Message):
 
     if invited_list:
         users = ", ".join(invited_list)
-        await message.reply_text(f"👥 {inviter} ɪɴᴠɪᴛᴇᴅ {users} ᴛᴏ ᴛʜᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ. 😉")
+        await message.reply_text(f"Oru manuchan koopuduran heh {inviter} ɪɴᴠɪᴛᴇᴅ {users} Vc ku vangaleey 😉")
 
 
 @app.on_message(filters.command("leavegroup") & filters.user(OWNER_ID))
 async def leave_group(_, message: Message):
-    await message.reply_text("👋 **ʟᴇᴀᴠɪɴɢ ᴛʜɪs ɢʀᴏᴜᴘ...**")
+    await message.reply_text("👋 **Bye... buyee Leaving the group...**")
     await app.leave_chat(chat_id=message.chat.id, delete=True)
