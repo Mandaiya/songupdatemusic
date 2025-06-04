6d661b839639ecea31b6f1f64d20be7a44d62ca9
natha <108113193+Mandaiya@users.noreply.github.com>
2025-06-04 06:44:09 +0530

    Update welcome.py

--- ANNIEMUSIC/plugins/tools/welcome.py
+++ ANNIEMUSIC/plugins/tools/welcome.py
@@ -19,7 +19,7 @@ from pyrogram.enums import ParseMode
 from pyrogram import *
 from pyrogram.types import *
 from logging import getLogger
-import admin_filter
+import admin_filters
 import os
 from PIL import ImageDraw, Image, ImageFont, ImageChops
 from pyrogram import *
